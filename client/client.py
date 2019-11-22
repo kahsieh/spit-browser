0455e3653b11abddd6d6b34b8bc24fc53450241e
Kevin Hsieh <kevin899@gmail.com>
2019-11-21 16:54:55 -0800

    Skeleton code

---
+++ client/client.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""
+SPIT-Browser Client
+"""
+
+if __name__ == '__main__':
+  print('Hello world!')
---
+++ scheduler/scheduler.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""
+SPIT-Browser Scheduler
+"""
+
+if __name__ == '__main__':
+  print('Hello world!')
